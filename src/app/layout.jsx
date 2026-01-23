1cdd60837793dbb31b454e6951a74d9e9389bb68
prashantjinwal <prashantjinwal888@gmail.com>
2026-01-23 13:06:17 +0530

    Layout updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,14 @@
 import "./globals.css";
 import ParticleNetwork from "@/components/ParticleNetwork";
+import { Major_Mono_Display, Inter } from 'next/font/google';
+
+// Google Fonts setup
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const mono = Major_Mono_Display({ subsets: ['latin'], weight: '400', variable: '--font-mono' });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${mono.variable}`}>
       <body className="overflow-hidden">
         <ParticleNetwork />
         <main className="relative z-10">
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <section className="min-h-screen flex items-center justify-center text-white">
-      <h1 className="text-5xl font-semibold">Dot Orbit</h1>
+      <h1 className="text-[240px] text-[#edf2f4] font-[var(--font-mono)]">PRASHANT</h1>
+
+      
     </section>
   );
 }
